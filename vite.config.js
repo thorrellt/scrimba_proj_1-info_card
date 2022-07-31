e9abb50a16959c9cea673fd3e87b40d97de3a032
Thorrell Turner <thorrellt@gmail.com>
2022-07-30 22:58:44 -0400

    add build scripts for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  base: '/scrimba_proj_1-info_card/',
+  plugins: [react()],
+  build: {
+    outDir: 'build'
+  }
 })
